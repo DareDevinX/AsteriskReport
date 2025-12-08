90c70530f23959facfbc7a875085240f2b9b7048
DareDevinX <17685318+DareDevinX@users.noreply.github.com>
2025-12-08 19:03:28 +0100

    second draft of bitmap graph, includes axis and is drawn from bottom to top

--- AsteriskReport.Logic/Graph/BmpGenerator.cs
+++ AsteriskReport.Logic/Graph/BmpGenerator.cs
@@ -20,6 +20,9 @@ namespace AsteriskReport.Logic.Graph
             { BarColor.Green, new SolidBrush(Color.Green)}
         };
 
+        private Pen borderPen = new Pen(Color.Black, 1);
+        private Brush backgroundBrush = new SolidBrush(Color.White);
+
         public BmpGenerator(BarGraphConfig config)
         {
             this.config = config ?? throw new ArgumentNullException(nameof(config));
@@ -29,29 +32,34 @@ namespace AsteriskReport.Logic.Graph
         {
             var canvasWidth = bars.Count() * (config.BarWidth + config.HorizontalSpacing);
             var canvasHeight = (int)bars.Max(bar => bar.Segments.Sum(segment => segment.Height));
-            var bitmap = new Bitmap(canvasWidth, canvasHeight);
+            var bitmap = new Bitmap(canvasWidth + 100, canvasHeight + 100);
             var graphics = Graphics.FromImage(bitmap);
+            graphics.FillRectangle(backgroundBrush, 0, 0, canvasWidth + 100, canvasHeight + 100);
 
 
+            
+            graphics.DrawLine(borderPen, 50, canvasHeight, canvasWidth + 100, canvasHeight);
+            graphics.DrawLine(borderPen, 50, canvasHeight, 50, 0);
             var rects = new List<RectangleF>();
             foreach (var bar in bars)
             {
                 foreach (var segment in bar.Segments)
                 {
-                    var rect = createRectFromSegment(segment, bar.X);
+                    var rect = createRectFromSegment(segment, bar.X, canvasHeight);
                     var brush = brushesByColor[segment.Color];
                     graphics.FillRectangle(brush, rect);
+                    graphics.DrawRectangle(borderPen, rect);
                 }
             }
 
             bitmap.Save("output.bmp");
         }
 
-        private RectangleF createRectFromSegment(BarSegment segment, float x)
+        private RectangleF createRectFromSegment(BarSegment segment, float x, float canvasHeight)
         {
             return new RectangleF(
-                x * (config.BarWidth + config.HorizontalSpacing),
-                segment.Y,
+                x * (config.BarWidth + config.HorizontalSpacing) + 100,
+                canvasHeight - segment.Height - segment.Y,
                 config.BarWidth,
                 segment.Height);
         }
---
+++ AsteriskReport.Tests/IntegrationTests/BmpGeneratorTests.cs
@@ -0,0 +1,47 @@
+﻿using AsteriskReport.Contracts.Config;
+using AsteriskReport.Contracts.DTOs;
+using AsteriskReport.Logic.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteriskReport.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class BmpGeneratorTests
+    {
+        private BmpGenerator sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            var config = new BarGraphConfig()
+            {
+                MaxBarSegmentLength = 100,
+                BarWidth = 10,
+                HorizontalSpacing = 5,
+                MinBarSegmentLength = 2
+
+            };
+            this.sut = new BmpGenerator(config);
+        }
+
+        [Test]
+        public void GenerateBmpImage_ShouldGenerateImage()
+        {
+            // Arrange
+            var segments = new BarSegment[]
+            {
+                new BarSegment(0, 10, BarColor.Red),
+                new BarSegment(10, 10, BarColor.Yellow),
+                new BarSegment(20, 10, BarColor.Green),
+            };
+            var bar = new Bar(segments, 0, 10);
+
+            // Act
+            this.sut.GenerateBmpImage(new[] { bar });
+        }
+    }
+}
